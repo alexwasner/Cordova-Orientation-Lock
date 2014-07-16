ee03aa53729a2831eaf3e6ab578aab590f3d403a
alexwasner <alex.wasner@gmail.com>
2014-07-15 22:52:07 -0500

    Removed un-needed semicolons causing issues

--- www/OrientationLock.js
+++ www/OrientationLock.js
@@ -6,19 +6,19 @@ OrientationLock.prototype.lock = function(success, fail){
   this.success = success;
   this.fail = fail;
   cordova.exec(success, fail, "OrientationLock", "lock", [false]);
-};
+}
 
 OrientationLock.prototype.unlock = function(success, fail){
   this.success = success;
   this.fail = fail;
   cordova.exec(success, fail, "OrientationLock", "unlock", [false]);
-};
+}
 
 OrientationLock.prototype.success = function(currentOri){
   if(this.success){
     this.success(currentOri);
   }
-};
+}
 
 cordova.addConstructor(function() {
     if(!window.plugins)        {
